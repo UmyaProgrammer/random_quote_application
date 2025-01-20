7c7c1227da8dcb4b8d909453564ddeeec8cbceec
Usame Alan <alanusame001@gmail.com>
2025-01-20 15:37:04 -0600

    Initial Commit

---
+++ main.py
@@ -0,0 +1,18 @@
+from dotenv import load_dotenv
+import requests
+import os
+
+load_dotenv()
+
+class Quote:
+    def __init__(self):
+        self.URL = os.getenv('url')
+        self.list_of_quotes = self.get_quotes()
+
+    def get_quotes(self):
+        response = requests.get(self.URL)
+        return response.json()
+
+if __name__ == '__main__':
+    quotes = Quote().list_of_quotes
+    print(quotes)
